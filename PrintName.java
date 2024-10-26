30d68f87cfe3048002464938d23372c5be4131cd
rony2164 <rony2164@gmail.com>
2024-10-26 06:49:55 +0000

    Create PrintName.java
    Print Numbers

---
+++ PrintName.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class printNum
+{
+    public static void main(String[] args) {
+       int i;
+       for (i=1;i<=10;i++)
+        System.out.println(i);
+    }
+}
